535901b90ddce2d102fc596abfe87daa1968a471
thales <thales.ume@gmail.com>
2023-03-08 22:13:30 -0300

    user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { UserResolver } from './resolver/user.resolver';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: true,
-  }),],
+  }), 
+  UserModule,
+],
   controllers: [],
   providers: [UserResolver],
 })
