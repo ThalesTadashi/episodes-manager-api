71605565ca0f1bb9bbbeb59e86eddd29e5bbc6dd
thales <thales.ume@gmail.com>
2023-03-10 23:09:05 -0300

    test

---
+++ ormconfig.js
@@ -0,0 +1,11 @@
+module.exports =
+{
+    "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "develop",
+      "password": "",
+      "database": "nest_api",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,34 @@
 import { ApolloDriverConfig, ApolloDriver } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { join } from "path";
 import { UserModule } from "./user/user.module";
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
  
-  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+  imports: [
+    TypeOrmModule.forRoot({
+      
+        type: "mysql",
+          host: "localhost",
+          port: 3306,
+          username: "develop",
+          password: "",
+          database: "nest_api",
+          entities: ["dist/**/*.entity{.ts,.js}"],
+          synchronize: true
+    
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
     driver: ApolloDriver,
     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
   }), 
+  ConfigModule.forRoot({
+    isGlobal: true
+  }),
   UserModule,
 ],
   controllers: [],
