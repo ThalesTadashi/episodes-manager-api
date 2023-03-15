815633ab818ab41a142016aa9b7a9cd01b3ceae9
thales <thales.ume@gmail.com>
2023-03-14 21:12:13 -0300

    start

---
+++ ormconfig.js
@@ -0,0 +1,12 @@
+import { User } from "./user/user.entity";
+module.exports =
+{
+    "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "root",
+      "password": "mypassword",
+      "database": "nest_api",
+      "entities": ["dist/**/*.entity{.ts,.js}"],
+      "synchronize": true
+}
