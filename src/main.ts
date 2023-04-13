2859f9cd48e620465a373f377484bf3eb4395a80
thales <thales.ume@gmail.com>
2023-04-12 21:20:53 -0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+  app.use(cors({ origin: 'http://localhost:4200' }));
   await app.listen(3000);
+    //CORS 
+    const whitelist = [
+      'http://localhost:4200',
+      'http://localhost:3000',
+    ];
+  
+    app.enableCors({
+      origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+    });
 }
 bootstrap();
